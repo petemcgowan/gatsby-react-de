7fa0729f038e4730da1dbad8d191a0d48952ee62
Pete <peteburquette@gmail.com>
2021-03-19 15:37:21 +0000

    layout seo menu

---
+++ src/data/MenuData.js
@@ -0,0 +1,8 @@
+/* Contains text elements which can be mapped*/
+
+export const menuData = [
+  {title: "About", link: "/about"},
+  {title: "Trips", link: "/trips"},
+  {title: "Careers", link: "/careers"},
+  {title: "Contact", link: "/contact"},
+]
