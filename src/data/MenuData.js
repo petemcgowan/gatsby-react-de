403c91c2d121a6721ec68a376b764600416555e2
Pete <peteburquette@gmail.com>
2021-04-14 14:11:27 +0100

    removing POC files

--- src/data/MenuData.js
+++
@@ -1,8 +0,0 @@
-/* Contains text elements which can be mapped*/
-
-export const menuData = [
-  { title: "About", link: "/about" },
-  { title: "Pages", link: "/pages" },
-  { title: "Careers", link: "/careers" },
-  { title: "Contact", link: "/contact" },
-]
