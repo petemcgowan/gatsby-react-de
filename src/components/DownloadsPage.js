3db347965a9b9b238dd23b5349169cab1ee595c0
Pete <peteburquette@gmail.com>
2021-04-23 20:40:51 +0100

    downloads mods

---
+++ src/components/DownloadsPage.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react"
+import MixTable from "./MixTable.js"
+import { Mixes } from "../data/Mixes"
+import { djMixes } from "../data/djMixes"
+import SearchBar from "./SearchBar.js"
+import "./Downloads.css"
+
+const DownloadsPage = () => {
+  const [filterText, setFilterText] = useState("")
+  const [filterDjMixText, setFilterDjMixText] = useState("")
+
+  function handleFilterTextInput(filterText) {
+    setFilterText(filterText)
+  }
+
+  function handleDJFilterTextInput(filterText) {
+    setFilterDjMixText(filterText)
+  }
+
+  return (
+    <div className="Download-card">
+      <h4 className="Download-card">Remixes / Edits</h4>
+      <SearchBar
+        filterText={filterText}
+        onFilterTextInput={handleFilterTextInput}
+      />
+      <MixTable mixes={Mixes} filterText={filterText} />
+
+      <h4>DJ Mixes</h4>
+      <SearchBar
+        filterText={filterDjMixText}
+        onFilterTextInput={handleDJFilterTextInput}
+      />
+      <MixTable mixes={djMixes} filterText={filterDjMixText} />
+    </div>
+  )
+}
+
+export default DownloadsPage
