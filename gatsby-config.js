cf428baa532141f8ee1ab7a9cc2c749c7f6300c3
Pete <peteburquette@gmail.com>
2021-05-26 17:04:31 +0100

    moving podcast rss for S3 deploy + fav ico change

--- gatsby-config.js
+++ gatsby-config.js
@@ -128,7 +128,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/cropped-Doggie-Lynne-lo-fi-32x32.jpeg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
