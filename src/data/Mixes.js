50c417b8b3e5c48e9dbcf4cea6fb0a8f164e9347
Pete <peteburquette@gmail.com>
2021-07-12 12:35:53 +0100

    new edit, reordered mix content

--- src/data/Mixes.js
+++ src/data/Mixes.js
@@ -1,5 +1,20 @@
 export const Mixes = [
   {
+    artist: "BT Express",
+    song: "Do It till youre satisfied (rmx)",
+    link: "http://bit.ly/2sWYWoJ",
+  },
+  {
+    artist: "James Brown",
+    song: "Mind Power edit (v3, Jul 2021)",
+    link: "https://bit.ly/3hxzrkY",
+  },
+  {
+    artist: "The Meters",
+    song: "Just Kissed My Baby (rmx)",
+    link: "http://bit.ly/2Pr2rvf",
+  },
+  {
     artist: "Missy Elliott",
     song: "Ultrafields House (Remix 3 Jan 2021)",
     link: "https://bit.ly/3rRvk6q",
@@ -10,14 +25,19 @@ export const Mixes = [
     link: "https://bit.ly/3oriUka",
   },
   {
-    artist: "BT Express",
-    song: "Do It till youre satisfied (rmx)",
-    link: "http://bit.ly/2sWYWoJ",
+    artist: "Sir Joe Quarterman, Free Soul",
+    song: "How High (rmx)",
+    link: "http://bit.ly/2TEBULA",
   },
   {
-    artist: "The Meters",
-    song: "Just Kissed My Baby (rmx)",
-    link: "http://bit.ly/2Pr2rvf",
+    artist: "The Blackbyrds",
+    song: "Do It Fluid (rmx)",
+    link: "http://bit.ly/2GK6JN1",
+  },
+  {
+    artist: "Hamilton Bohannon",
+    song: "Red Bone (edit)",
+    link: "http://bit.ly/2TVoTx6",
   },
   {
     artist: "Midnight Movers",
@@ -35,6 +55,16 @@ export const Mixes = [
     link: "http://bit.ly/2PpOdux",
   },
   {
+    artist: "Ramsey Lewis",
+    song: "Sun Goddess (rmx)",
+    link: "http://bit.ly/2J6xESX",
+  },
+  {
+    artist: "William DeVaughn (feat Gil Scott Heron)",
+    song: "Be Thankful For What You Got (edit)",
+    link: "http://bit.ly/2AlfszM",
+  },
+  {
     artist: "Public Enemy",
     song: "Power To the People (rmx)",
     link: "http://bit.ly/2P1YM81",
@@ -49,46 +79,9 @@ export const Mixes = [
     song: "Bubble and Slide (edit)",
     link: "http://bit.ly/2LBxDad",
   },
-
   {
     artist: "The Everyday People",
     song: "Funky Generation (rmx)",
     link: "http://bit.ly/2ksxxqX",
-  },
-  {
-    artist: "Ramsey Lewis",
-    song: "Sun Goddess (rmx)",
-    link: "http://bit.ly/2J6xESX",
-  },
-  {
-    artist: "James Brown",
-    song: "Mind Power (edit)",
-    link: "http://bit.ly/2Ir71Zr",
-  },
-  {
-    artist: "James Brown2",
-    song: "Mind Power Interlude (rmx)",
-    link: "http://bit.ly/2VXL07O",
-  },
-
-  {
-    artist: "Sir Joe Quarterman, Free Soul",
-    song: "How High (rmx)",
-    link: "http://bit.ly/2TEBULA",
-  },
-  {
-    artist: "The Blackbyrds",
-    song: "Do It Fluid (rmx)",
-    link: "http://bit.ly/2GK6JN1",
-  },
-  {
-    artist: "Hamilton Bohannon",
-    song: "Red Bone (edit)",
-    link: "http://bit.ly/2TVoTx6",
-  },
-  {
-    artist: "William DeVaughn (feat Gil Scott Heron)",
-    song: "Be Thankful For What You Got (edit)",
-    link: "http://bit.ly/2AlfszM",
   },
 ]
