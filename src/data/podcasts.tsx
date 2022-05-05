78b7551bdb60ad614c327f91497a736d9154ed91
Pete <peteburquette@gmail.com>
2022-05-05 19:24:51 +0100

    Added amazon podcast, interfaces

--- src/data/podcasts.tsx
+++ src/data/podcasts.tsx
@@ -11,28 +11,44 @@ export const podcasts = [
         href:
           "https://podcasts.apple.com/lc/podcast/underground-house-funk-techno/id1445101714",
         title: "Apple Podcasts",
-      },
-      {
-        href:
-          "https://play.pocketcasts.com/web/podcasts/share?id=c42542a0-d999-0136-3249-08b04944ede4",
-        title: "Pocket Casts",
-      },
-      {
-        href:
-          "https://www.stitcher.com/s?eid=57501102&autoplay=1/",
-        title: "Stitcher",
-      },
-      {
-        href:
-          "http://tun.in/pjf12",
-        title: "TuneIn",
+        imgSrc: "../img/btnApplePodcasts.svg",
+        imgText: "",
       },
       {
         href:
           "https://music.amazon.com/podcasts/8f127a9f-adb9-4f8a-87b4-389f9f0b788f/underground-house-funk-techno",
         title: "Amazon Music",
+        imgSrc: "../img/btnAmazonMusic.svg",
+        imgText: "",
       },
-
+      {
+        href:
+          "https://play.pocketcasts.com/web/podcasts/share?id=c42542a0-d999-0136-3249-08b04944ede4",
+        title: "Pocket Casts",
+        imgSrc: "../img/btnPocketCasts.svg",
+        imgText: "",
+      },
+      {
+       href:
+         "https://www.stitcher.com/s?eid=57501102&autoplay=1/",
+        title: "Stitcher",
+        imgSrc: "../img/btnSticherListen.jpeg",
+        imgText: "",
+      },
+      {
+        href:
+          "http://tun.in/pjf12",
+        title: "TuneIn",
+        imgSrc: "../img/btnTuneIn.svg",
+        imgText: "",
+      },
+      {
+        href:
+          "https://youtube.com/playlist?list=PLMniNzct9KeblWL-oKL4Pc9m3yFkRzhFT",
+        title: "YouTube",
+        imgSrc: "../img/btnYouTube.svg",
+        imgText: "",
+      },
     ],
   },
   {
@@ -46,26 +62,44 @@ export const podcasts = [
         href:
           "https://podcasts.apple.com/ie/podcast/pure-techno-underground/id1527885709",
         title: "Apple Podcasts",
-      },
-      {
-        href:
-          "https://pca.st/podcast/9b009de0-c16d-0138-e716-0acc26574db2",
-        title: "Pocket Casts",
-      },
-      {
-        href:
-          "https://www.stitcher.com/s?fid=564416",
-        title: "Stitcher",
-      },
-      {
-        href:
-          "http://tun.in/pj0lb",
-        title: "TuneIn",
+        imgSrc: "../img/btnApplePodcasts.svg",
+        imgText: "",
       },
       {
         href:
           "https://music.amazon.com/podcasts/0e6c9c92-07e6-499f-939d-db64ad8a3d20",
         title: "Amazon Music",
+        imgSrc: "../img/btnAmazonMusic.svg",
+        imgText: "",
+      },
+      {
+        href:
+          "https://youtube.com/playlist?list=PLJL85AxPFGSOhzdPS4HzSX4CFS8dhisJd",
+        title: "YouTube",
+        imgSrc: "../img/btnYouTube.svg",
+        imgText: "",
+      },
+      {
+        href:
+          "https://pca.st/podcast/9b009de0-c16d-0138-e716-0acc26574db2",
+        title: "Pocket Casts",
+        imgSrc: "../img/btnPocketCasts.svg",
+        imgText: "",
+      },
+      {
+        href:
+          "https://www.stitcher.com/s?fid=564416",
+        title: "Stitcher",
+        imgSrc: "../img/btnSticherListen.jpeg",
+        imgText: "",
+
+      },
+      {
+        href:
+          "http://tun.in/pj0lb",
+        title: "TuneIn",
+        imgSrc: "../img/btnTuneIn.svg",
+        imgText: "",
       },
     ],
   },
--- src/interfaces/PodcastInterfaces.ts
+++ src/interfaces/PodcastInterfaces.ts
@@ -1,17 +1,19 @@
 export interface PodcastLinkType {
-  href: string;
-  title: string;
+  href: string
+  title: string
+  imgSrc: string
+  imgText: string
 }
 
 export interface PodcastType {
-  name: string;
-  imgHref: string;
-  imgSrc: string;
-  imgAlt: string;
-  techno: boolean;
-  links: PodcastLinkType[];
+  name: string
+  imgHref: string
+  imgSrc: string
+  imgAlt: string
+  techno: boolean
+  links: PodcastLinkType[]
 }
 
 export interface PodcastProps {
-  podcastInfo: PodcastType;
+  podcastInfo: PodcastType
 }
